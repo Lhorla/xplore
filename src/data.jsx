b7225b0d5b3b09a316fa9b7c9b46bb468a1fc94a
Lhorla <teteelola@gmail.com>
2022-09-23 23:35:13 +0100

    Update data.jsx

--- src/data.jsx
+++ src/data.jsx
@@ -8,12 +8,12 @@ import imgRufus from './images/rufus-and-bee.jpg'
 export default [
     {
         id: 1,
-        title: "Tarkwa Bay",
+        title: "Tarkwa Bay Beach",
         location: "Lagos",
         googleMapsUrl: "https://goo.gl/maps/RJAGPdeZDELuUok39",
-        openingHours: "8am",
-        closingHours: "5pm",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum magni explicabo in obcaecati totam expedita molestias molestiae fugiat modi fuga.",
+        openingHours: "8AM",
+        closingHours: "5PM",
+        description: "Unlike most beaches in Lagos that are becoming more groovy than peaceful, Tarkwa Bay, in its elevating beauty, pride itself as a free haven that is not usually noisy or crowded; making it a unique place to have some quiet time with loved ones or a fun get-together with friends.",
         imageUrl: imgTarkwa
     },
     {
@@ -21,9 +21,9 @@ export default [
         title: "Rufus and Bee",
         location: "Lagos",
         googleMapsUrl: "https://goo.gl/maps/HxqELpRvofDj2vWAA",
-        openingHours: "8am",
-        closingHours: "5am",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        openingHours: "8AM",
+        closingHours: "5PM",
+        description: "Experience fun like never before. Traditional and modern, high-tech games with an amazing sport bar and boutique style bowling alleys for private events, social crowds and families.",
         imageUrl: imgRufus
     },
     {
@@ -31,9 +31,9 @@ export default [
         title: "Agodi Gardens",
         location: "Oyo",
         googleMapsUrl: "https://goo.gl/maps/rLTx8bx1QuahcBUV6",
-        openingHours: "8am",
-        closingHours: "5pm",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        openingHours: "8AM",
+        closingHours: "5PM",
+        description: "Agodi Gardens contains an indigenous forest, zoo, a lake and tree plantations. The park is surrounded by the bustling and sprawling metropolitan area, it stands out as a green lung in the surrounding urban landscape with great recreational potential.",
         imageUrl: imgAgodi
     },
     {
@@ -41,9 +41,9 @@ export default [
         title: "Omu Resort",
         location: "Lagos",
         googleMapsUrl: "https://goo.gl/maps/ZdN8WBUWN2CbfsuYA",
-        openingHours: "8am",
-        closingHours: "5pm",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        openingHours: "8AM",
+        closingHours: "5PM",
+        description: "The Omu Resort is a recreational center with a lot of fun activities, which allows guests to experience and be inspired by the wonders of nature. It houses a zoo; Freshwater SeaWorld; a Wax museum; Amusement rides; Go-karting; Quad biking and more.",
         imageUrl: imgOmu
     },
     {
@@ -51,9 +51,9 @@ export default [
         title: "Olusegun Obasanjo Presidential Library",
         location: "Ogun",
         googleMapsUrl: "https://goo.gl/maps/rnTaMYXVNhRWDQhi9",
-        openingHours: "8am",
-        closingHours: "5pm",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        openingHours: "8AM",
+        closingHours: "5PM",
+        description: "OOPL is a full community on 32 hectares of the earth’s space consisting of igneous Rocks and plateau-like formation, It encompasses the Presidential Library and Museum, tourist attractions such as the Zoological gardens, Amphitheater, The Adire and African Fabrics Centre",
         imageUrl: imgOopl
     },
     {
@@ -61,9 +61,9 @@ export default [
         title: "Lekki Conservation Centre",
         location: "Lagos",
         googleMapsUrl: "https://goo.gl/maps/CaJpA8mC7PTAJ27f9",
-        openingHours: "8am",
-        closingHours: "5pm",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        openingHours: "8AM",
+        closingHours: "5PM",
+        description: "It is an oasis of nature within the urban chaos of Lagos with a tree house, nature station, bird hide, swamp look-out station, rotunda, 96-seater gazebos, 6- and 8-seater family huts, floor games (ludo, snake and ladder, draught, chess), and the suspended canopy walkway that rises over the reserve is the longest in all of Africa.",
         imageUrl: imgLcc
     }
 ]
