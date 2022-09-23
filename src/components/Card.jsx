60b25a299adef3bf525395476a7666b473d8622f
Lhorla <teteelola@gmail.com>
2022-09-23 23:34:36 +0100

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,22 @@
 import React from "react";
 
 function Card(props) {
+  
+  /*
+  let time = new Date();
+  let hour = time.getHours();
+  let prepand===hour >= 12 ? " PM " : " AM ";
+  
+  if (hour = 0 && prepand===" PM ") {
+    hour = 12;
+    prepand = " PM ";
+  }
+  if (hour===0 && prepand===" AM ") {
+    hour = 12;
+    prepand = " AM ";
+  } */
+
+
   return (
     <div className="card-container">
       <div>
@@ -13,11 +29,13 @@ function Card(props) {
         </div>
 
         <h1>{props.title}</h1>
-        <p>{props.openingHours}-{props.closingHours}</p>
+        <p>
+          {props.openingHours}-{props.closingHours}
+        </p>
         <p>{props.description}</p>
       </div>
     </div>
   );
 }
 
-export default Card
+export default Card;
