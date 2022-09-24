604f22d64a9084a2f3c1271b684067af0fb6fda6
Lhorla <teteelola@gmail.com>
2022-09-24 01:51:23 +0100

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div className="footer">
+            <small>Coded by <a href="https://github.com/lhorla" target="_blank">Lhorla</a></small>
+        </div>
+    )
+}
+
+export default Footer;
