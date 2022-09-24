1290db702a15d782e84ead310fad182b8a513e80
Lhorla <teteelola@gmail.com>
2022-09-24 01:51:29 +0100

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { FaGlobe } from "react-icons/fa";
 
 function Nav() {
     return (
         <div className="nav">
-            <h1>Travel Tour</h1>
+            <h1><FaGlobe className="nav-icon"/>Travel Journal</h1>
         </div>
     )
 }
