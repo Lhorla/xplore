dbf0b2a78e5099b320651293a8ce95b16f2abd7c
Lhorla <teteelola@gmail.com>
2022-09-24 01:51:13 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,21 @@
-import './App.css'
-import Card from './components/Card'
-import Nav from './components/Nav'
-import data from './data'
+import "./App.css";
+import Card from "./components/Card";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import data from "./data";
 
 function App() {
-  const travel = data.map(list => {
-    return (
-      <Card 
-        key={list.id}
-        {...list}
-      />
-    )
-  })
+  const travel = data.map((list) => {
+    return <Card key={list.id} {...list} />;
+  });
 
   return (
     <div className="App">
-        <Nav />
-        <div>
-          {travel}
-        </div>
+      <Nav />
+      <div>{travel}</div>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
